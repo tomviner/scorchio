12c6c6a6c7959d7fbc51b8774b902fa09fc73b4f
TomV <tom@viner.tv>
2013-07-05 18:33:36 +0200

    add Shell class

--- tank.py
+++ tank.py
@@ -1,6 +1,24 @@
-from game import draw_world
+#from game import draw_world
 
 from Tkinter import *
+
+
+
+class Shell(object):
+    g = 9.8
+
+    def __init__(self, start_x, start_y,
+        speed_x, speed_y):
+        self.x = start_x
+        self.y = start_y
+        self.speed_x = speed_x
+        # never changes
+        self.speed_y = speed_y
+
+    def position_at_time(t):
+        return (0.5 * self.g * t**2
+            + self.initial_speed
+            + self.start_y)
 
 class Tank(object):
 
@@ -12,14 +30,17 @@ class Tank(object):
     def coords(self):
         return (self.x, self.y)
 
+    def fire(self, angle):
+        pass
+
 
 class GameLoop(object):
     def tick(self):
 
-        draw_world(
-            tank_1=t1.coords,
-            tank_2=t2.coords,
-        )
+        # draw_world(
+        #     tank_1=t1.coords,
+        #     tank_2=t2.coords,
+        # )
 
         self.t += 1
         self.root.after(20, self.tick)
@@ -38,6 +59,14 @@ class GameLoop(object):
         self.root.after(2000, self.tick)
         self.root.mainloop()
 
+        frame.bind("<Key>", key)
+        frame.bind("<Button-1>", callback)
+
+        t1 = Tank(100)
+        t2 = Tank(700)
+
+    def key(event):
+        print "pressed", repr(event.char)
 
 def main():
     g = GameLoop()
@@ -45,4 +74,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
